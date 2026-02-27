5325f3a7be6c160e778d328328e9ad5c393544da
Krish <3dstudent1997@gmail.com>
2026-02-28 00:04:15 +0530

    date

---
+++ catalogue.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ./common.sh
+
+app_name="catalogue"
+
+check_root
+
+app_setup
+
+Nodejs_setup
+
+System_ctl
+
+cp $SCRIPTDIR/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "mongodb-org install"
+
+INDEX=$(mongosh --host $MONGODBIP --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -ne 0 ]; then
+    mongosh --host $MONGODBIP </app/db/master-data.js
+else
+    echo -e "Product already exist... $Y skipping $N"
+fi
+
+Print_total_time
--- common.sh
+++ common.sh
@@ -8,6 +8,10 @@ G="\e[32m" #green
 Y="\e[33m" #yellow
 N="\e[0m" #normal
 
+
+SCRIPTDIR=$PWD
+MONGODBIP="mongodb.krishsky.online"
+
 Start_time=$(date +%s)
 
 mkdir -p $LOGS_FOLDER
@@ -30,6 +34,68 @@ VALIDATE(){
         echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ..... is $G Success $N" | tee $LOGS_FILE
     fi
 }
+
+
+Nodejs_setup(){
+
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disable Nodejs"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "enable Nodejs"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "install Nodejs"  
+
+    npm install  &>>$LOGS_FILE
+    VALIDATE $? "npm install"  
+}
+
+    app_setup(){
+            id roboshop
+
+        if [ $? -ne 0 ]; then   
+            useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+            VALIDATE $? "System User created"
+        else
+            echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+       fi
+
+        mkdir -p /app 
+        VALIDATE $? "app created"
+
+        curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip 
+        VALIDATE $? "$app_name download"
+
+        cd /app 
+        VALIDATE $? "Moving to app directory"
+
+        rm -rf /app/*
+        VALIDATE $? "removing existing code"
+
+        unzip /tmp/$app_name.zip
+        VALIDATE $? "Unzip code"
+
+    }
+
+    
+    cp $SCRIPTDIR/catalogue.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Copying $app_name.service file"
+
+    System_ctl(){
+    systemctl daemon-reload
+    systemctl enable $app_name
+    systemctl start $app_name
+    VALIDATE $? "reload enable Start $app_name"
+    }
+
+    Restart_app(){
+        systemctl restart $app_name
+        VALIDATE $? "Restarting $app_name"
+    }
+
+
+
 
 Print_total_time(){
 End_time=$(date +%s)
