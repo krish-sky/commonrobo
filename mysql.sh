07f432495f8cb48420d4fd58500207de36bbd186
Krish <3dstudent1997@gmail.com>
2026-03-01 13:04:34 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -4,7 +4,7 @@ source ./common.sh
 
 check_root
 
-app_name="mysql"
+app_name="mysqld"
 
 dnf install mysql-server -y
 VALIDATE $? "Installing Mysql"
@@ -12,5 +12,8 @@ VALIDATE $? "Installing Mysql"
 
 mysql_secure_installation --set-root-pass RoboShop@1
 VALIDATE $? "Password setup"
+
+systemctl enable $app_name
+systemctl start $app_name
 
 Print_total_time
