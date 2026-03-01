ce1eebbd1d10e372bfb6c3bff321568c32715182
Krish <3dstudent1997@gmail.com>
2026-03-01 13:38:05 +0530

    mysql

--- shipping.sh
+++ shipping.sh
@@ -16,12 +16,12 @@ System_ctl
 dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "install mysql"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOGS_FILE
 
 if [ $? -ne 0 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
 else  
     echo -e "Database already exist....$Y SKIPPING $N"
 fi
