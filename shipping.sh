2dc4defff9eb151cddbaf75684e03611ecbb9ed3
Krish <3dstudent1997@gmail.com>
2026-03-01 13:32:39 +0530

    mysql

--- shipping.sh
+++ shipping.sh
@@ -9,3 +9,24 @@ check_root
 app_setup
 
 Java_setup
+
+Service_file
+System_ctl
+
+dnf install mysql -y &>>$LOGS_FILE
+VALIDATE $? "install mysql"
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+
+if [ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+else  
+    echo -e "Database already exist....$Y SKIPPING $N"
+fi
+
+systemctl restart shipping
+VALIDATE $? "Shipping Restart"
+
+Print_total_time
